037126893407e4c405009f256535220c9f2df698
Khoa Nguyen <khoaanguyen@kms-technology.com>
2015-05-14 18:36:50 +0700

    commit all change

--- GitChallenge/Program.cs
+++ GitChallenge/Program.cs
@@ -10,21 +10,7 @@ namespace GitChallenge
     {
         static void Main(string[] args)
         {
-            _randomizedNames = DoRandomizeList(GetNames());
-            _randomizedNickNames = DoRandomizeList(GetNickNames());
-
-            foreach ( var name in _randomizedNames)
-            {
-                foreach (var nickName in _randomizedNickNames)
-                {
-                    if(name.Key == nickName.Key)
-                    {
-                        Console.WriteLine(string.Format("Name: '{0}' has nickname '{1}' ", name.Value, nickName.Value));
-                    }
-                }
-            }
-
-            Console.ReadKey();
+            
         }
 
 
@@ -35,47 +21,19 @@ namespace GitChallenge
 
         private static IList<string> GetNames()
         {
-            var names = new List<string>();
-
-            names.Add("Khoa");
-            names.Add("Khoa1");
-            names.Add("Khoa2");
-            names.Add("Khoa3");
-
-            return names;
+            return null;
         }
 
 
         private static IList<string> GetNickNames()
         {
-            var nickNames = new List<string>();
-            nickNames.Add("My nickname");
-            nickNames.Add("My nickname1");
-            nickNames.Add("My nickname2");
-            nickNames.Add("My nickname3");
-
-            return nickNames;
+            return null;
         }
 
 
         private static IList<KeyValuePair<int, string>> DoRandomizeList(IList<string> list)
         {
             var result = new List<KeyValuePair<int, string>>();
-            Random rand = new Random();
-
-            var check = new HashSet<int>();
-            foreach (var item in list)
-            {
-
-                int curValue = rand.Next(1, list.Count + 1);
-                while (check.Contains(curValue))
-                {
-                    curValue = rand.Next(1, list.Count + 1);
-                }
-                result.Add(new KeyValuePair<int, string>(curValue, item));
-
-                check.Add(curValue);
-            }
 
             return result;
         }
